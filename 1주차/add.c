7d8e7489602cc58ae51ac29840724eaf6c2717b7
benjioh5 <benjioh5@gmail.com>
2016-09-26 20:26:46 +0900

    1주차 과제 업데이트

---
+++ 1주차/add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   int a, b, c;
+   a = 1;
+   b = 2;
+   c = a + b;
+   printf("%d\n", c);
+   return 0;
+}
---
+++ 1주차/add2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   int a, b, c;
+   a = 1;
+   b = 2;
+   c = a + b;
+   printf("%d + %d = %d\n", a, b, c);
+   return 0;
+}
